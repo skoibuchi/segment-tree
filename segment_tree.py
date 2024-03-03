3ccb88a027cdc9ddc1b5681a6720fa11eb595ef0
satokoi <satoshi@west.sd.keio.ac.jp>
2024-03-03 06:10:24 +0000

    update

--- segment_tree.py
+++ segment_tree.py
@@ -1,36 +1,36 @@
 import math
 class SegmentTree:
     def __init__(self, array, func, ide_ele):
-        n = len(array)
-        self.num = 2**(n-1).bit_length()
+        self.num = len(array)
         self.ide_ele = ide_ele
         self.segfunc = func
         self.tree = [self.ide_ele]*(self.num*2)
-        for i in range(n):
-            self.tree[self.num-1+i] = array[i]
-        for i in range(self.num-2, -1, -1):
-            self.tree[i] = self.segfunc(self.tree[i*2+1], self.tree[i*2+2])
+        for i in range(self.num):
+            self.tree[self.num+i] = array[i]
+        for i in range(self.num-1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[i*2], self.tree[i*2+1])
     
     def update(self, k, x):
-        k += self.num-1
+        k += self.num
         self.tree[k] = x
         while k:
-            k = (k-1)//2
-            self.tree[k] = self.segfunc(self.tree[k*2+1], self.tree[k*2+2])
+            k = k // 2
+            self.tree[k] = self.segfunc(self.tree[k*2], self.tree[k*2+1])
     
     def query(self, l, r):
-        l += self.num-1
-        r += self.num-1
-        ret = self.ide_ele
-        while l<r:
-            if l&1==0:
-                ret = self.segfunc(ret, self.tree[l])
-            if r&1==0:
-                ret = self.segfunc(ret, self.tree[r-1])
+        l += self.num
+        r += self.num
+        res = self.ide_ele
+        while l<=r:
+            if l&1 == 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r&1 == 0:
+                res = self.segfunc(res, self.tree[r])
                 r -= 1
             l //=2
             r //=2
-        return ret
+        return res
 
 class BIT:
     def __init__(self, array):
@@ -60,12 +60,12 @@ if __name__ == '__main__':
     array = [0,1,2,3,4,5,6,7,8,9,10]
     st = SegmentTree(array, sum_, 0)
     # print('num of tree:', st.num)
+    print('tree: ', st.tree)
+    print('query(2, 5): ', st.query(2, 5))
+    st.update(2, 5)
     # print('tree: ', st.tree)
-    print('query(1,4): ', st.query(1, 4))
-    st.update(2, 3)
-    # print('tree: ', st.tree)
-    print('query(1,4): ', st.query(1, 4))
+    print('query(2, 5): ', st.query(2, 5))
 
     bit = BIT(array)
     # print('tree: ', bit.tree)
-    print('query(1,4): ', bit.query(4) - bit.query(0))
+    print('query(2, 5): ', bit.query(5+1) - bit.query(1+1))
